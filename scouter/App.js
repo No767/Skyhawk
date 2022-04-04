692a2ef44ff0992c664853fc7991af244a4862cd
No767 <73260931+No767@users.noreply.github.com>
2022-04-03 22:11:45 -0700

    remove simdjson + add test script
    The bindings for Simdjson require an GCC compiler, which i don't have on windows

--- scouter/App.js
+++ scouter/App.js
@@ -1,14 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+
       <StatusBar style="auto" />
     </View>
   );
 }
+
+function getItem() {
+  axios.get('https://api.waifu.pics/sfw/waifu')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
+}
+
 
 const styles = StyleSheet.create({
   container: {
