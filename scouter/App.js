4f158251af17c5908e5820ebce2a40f072ca742b
No767 <73260931+No767@users.noreply.github.com>
2022-04-21 09:12:45 -0700

    Start work with the app

--- scouter/App.js
+++ scouter/App.js
@@ -1,36 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Text, Box, Button} from 'native-base';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
 
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+      <Text>Data Testing</Text>
+      <Button>test</Button>
+    </Box>
+  </NativeBaseProvider>
   );
 }
-
-function getItem() {
-  axios.get('https://api.waifu.pics/sfw/waifu')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .then(function () {
-    // always executed
-  });
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
