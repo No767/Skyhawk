f82e604f647f5d675d1ea1d21b2dd852b24e65db
No767 <73260931+No767@users.noreply.github.com>
2022-04-24 19:29:17 -0700

    Attempt to build navbar

--- scouter/App.js
+++ scouter/App.js
@@ -1,12 +1,24 @@
-import { NativeBaseProvider, Text, Box, Button} from 'native-base';
+import { NativeBaseProvider,  Box, Button} from 'native-base';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { View, Text } from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
 export default function App() {
   return (
-
-    <NativeBaseProvider>
-    <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
-      <Text>Data Testing</Text>
-      <Button>test</Button>
-    </Box>
-  </NativeBaseProvider>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
